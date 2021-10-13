d7bed7785620339ef4db7765c35948eb5fb8cec9
petrapmf <pakrap@pmfst.hr>
2021-10-13 13:29:18 +0200

    Vjezbe 1

--- Vjezbe_1/Zadatak_6.cpp
+++ Vjezbe_1/Zadatak_6.cpp
@@ -39,9 +39,11 @@ int main() {
     ElementaryParticle higgs("Higgs boson", 124.97, true);
     ElementaryParticle top("Top quark", 173.1, false);
 
-    ElementaryParticle Z("Z boson", 91.19, true);
-    ElementaryParticle* ptrZ;
-    ptrZ = &Z;
+   // ElementaryParticle Z("Z boson", 91.19, true);
+    //ElementaryParticle* ptrZ;
+    //ptrZ = &Z;
+
+    ElementaryParticle* ptrZ = new ElementaryParticle("Z boson", 91.19, true);
 
     //ElementaryParticle* ptrZ("Z boson", 91.19, true);
 
