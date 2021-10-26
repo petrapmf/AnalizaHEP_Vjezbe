73864865817475452f02bfb89699055626f76fc1
petrapmf <pakrap@pmfst.hr>
2021-10-26 23:38:53 +0200

    Added Zad3

--- Vjezbe_3/ElementaryParticle.cpp
+++ Vjezbe_3/ElementaryParticle.cpp
@@ -44,7 +44,7 @@ void ElementaryParticle::momentum(float px, float py, float pz) {
 
     energy = sqrt(pow(p, 2.) + pow(mass, 2.));
 
-    cout << "Energy: " << energy << endl;
+    //cout << "Energy: " << energy << endl;
 }
 
 void ElementaryParticle::transMomentum(float px, float py) {
@@ -60,81 +60,95 @@ void ElementaryParticle::bosonDecay(ElementaryParticle* p1, ElementaryParticle* 
         return;
     }
      
-    srand(time(NULL));
-    float higgsDecay = (float)rand() / RAND_MAX;
+    srand(time(NULL)); //za rand() fju
+    random_device rd; // random seed
+    mt19937 gen(rd()); // initialize pseudo-random number generator
+    uniform_real_distribution<float> dist(0.0, this->px); // range 
+    float higgsDecay = dist(gen);
     
     if (higgsDecay <= 0.214) { // 21.4% slucaja
-        cout << "Higgs boson decayed into W bosons." << endl;
+        //cout << "Higgs boson decayed into W bosons." << endl;
+        p1->name = "W boson";
+        p2->name = "W boson";
         // randNum = (min + 1) + (((float) rand()) / (float) RAND_MAX) * (max - (min + 1))
-        p1->px = (0.0 + 1.0) + (((float) rand()) / (float) RAND_MAX) * (2.0 - (0.0 + 1.0));
+        p1->px = (0.0 + 1.0) + (((float) rand()) / (float) RAND_MAX) * (this->px - (0.0 + 1.0));
         p2->px = 2.0 - p1->px;
 
-        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (1.0 - (0.0 + 1.0));
+        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->py - (0.0 + 1.0));
         p2->py = 1.0 - p1->py;
 
-        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (2.0 - (0.0 + 1.0));
+        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->pz - (0.0 + 1.0));
         p2->pz = 2.0 - p1->pz;
 
-        cout << "1st W boson ";
+        //cout << "1st W boson ";
         p1->momentum(p1->px, p1->py, p2->pz);
-        cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
-        cout << "2nd W boson "; 
+        //cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
+        //cout << "2nd W boson "; 
         p2->momentum(p2->px, p2->py, p2->pz);
-        cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
+        //cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
     }
     else if (0.214 < higgsDecay <= 0.278) { // 6.4% slucaja
-        cout << "Higgs boson decayed into tau leptons." << endl;
-        p1->px = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (2.0 - (0.0 + 1));
+        //cout << "Higgs boson decayed into tau leptons." << endl;
+        p1->name = "Tau lepton";
+        p2->name = "Tau lepton";
+
+        p1->px = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->px - (0.0 + 1));
         p2->px = 2.0 - p1->px;
 
-        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (1.0 - (0.0 + 1.0));
+        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->py - (0.0 + 1.0));
         p2->py = 1.0 - p1->py;
 
-        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (2.0 - (0.0 + 1.0));
+        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->pz - (0.0 + 1.0));
         p2->pz = 2.0 - p1->pz;
 
-        cout << "1st tau lepton ";
+        //cout << "1st tau lepton ";
         p1->momentum(p1->px, p1->py, p2->pz);
-        cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
-        cout << "2nd tau lepton ";
+        //cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
+        //cout << "2nd tau lepton ";
         p2->momentum(p2->px, p2->py, p2->pz);
-        cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
+        //cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
     }
     else if (0.278 < higgsDecay <= 0.304) { // 2.6% slucaja
-        cout << "Higgs boson decayed into Z bosons." << endl;
-        p1->px = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (2.0 - (0.0 + 1.0));
+        //cout << "Higgs boson decayed into Z bosons." << endl;
+        p1->name = "Z boson";
+        p2->name = "Z boson";
+
+        p1->px = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->px - (0.0 + 1.0));
         p2->px = 2.0 - p1->px;
 
-        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (1.0 - (0.0 + 1.0));
+        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->py - (0.0 + 1.0));
         p2->py = 1.0 - p1->py;
 
-        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (2.0 - (0.0 + 1.0));
+        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->pz - (0.0 + 1.0));
         p2->pz = 2.0 - p1->pz;
 
-        cout << "1st Z boson ";
+        //cout << "1st Z boson ";
         p1->momentum(p1->px, p1->py, p2->pz);
-        cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
-        cout << "2nd Z boson ";
+        //cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
+        //cout << "2nd Z boson ";
         p2->momentum(p2->px, p2->py, p2->pz);
-        cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
+        //cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
     }
     else {
-        cout << "Higgs boson decayed into b quarks." << endl;
-        p1->px = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (2.0 - (0.0 + 1.0));
+        //cout << "Higgs boson decayed into b quarks." << endl;
+        p1->name = "b quark";
+        p2->name = "b quark";
+
+        p1->px = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->px - (0.0 + 1.0));
         p2->px = 2.0 - p1->px;
 
-        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (1.0 - (0.0 + 1.0));
+        p1->py = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->py - (0.0 + 1.0));
         p2->py = 1.0 - p1->py;
 
-        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (2.0 - (0.0 + 1.0));
+        p1->pz = (0.0 + 1.0) + (((float)rand()) / (float)RAND_MAX) * (this->pz - (0.0 + 1.0));
         p2->pz = 2.0 - p1->pz;
 
-        cout << "1st b quark ";
+        //cout << "1st b quark ";
         p1->momentum(p1->px, p1->py, p2->pz);
-        cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
-        cout << "2nd b quark ";
+        //cout << "px = " << p1->px << "\tpy = " << p1->py << "\tpz = " << p1->pz << endl;
+        //cout << "2nd b quark ";
         p2->momentum(p2->px, p2->py, p2->pz);
-        cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
+        //cout << "px = " << p2->px << "\tpy = " << p2->py << "\tpz = " << p2->pz << endl;
     }
 
 }
--- Vjezbe_3/analyzer.cpp
+++ Vjezbe_3/analyzer.cpp
@@ -1,23 +1,42 @@
 #include "ElementaryParticle.h"
 #include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <random>
 
+using namespace std;
 using namespace EP;
 
-
 int main() {
-	ElementaryParticle higgs("Higgs boson", 124.97, true);
-	//ElementaryParticle top("Top quark", 173.1, false);
-	//ElementaryParticle* ptrZ = new ElementaryParticle("Z boson", 91.19, true);
-	higgs.px = 2.0;
-	higgs.py = 1.0;
-	higgs.pz = 2.0;
-	higgs.momentum(higgs.px, higgs.py, higgs.pz);
-    higgs.transMomentum(higgs.px, higgs.py);
+	//srand(time(NULL));
 
-	ElementaryParticle* particle1 = new ElementaryParticle();
-	ElementaryParticle* particle2 = new ElementaryParticle();
-	higgs.bosonDecay(particle1, particle2);
-	//top.bosonDecay(particle1, particle2);
+	random_device rd; // random seed
+	mt19937 gen(rd()); // initialize pseudo-random number generator
+	uniform_real_distribution<float> dist(-100.0, 100.0); // range 
 
+	int i;
+	ofstream MyFile;
+	MyFile.open("Analysis.txt");
+
+	MyFile << "1st NAME\tpx   py   pz\t\t\t2nd NAME\tpx   py   pz\n";
+
+	for (i = 0; i < 10000; i++) {
+		ElementaryParticle higgs("Higgs boson", 124.97, true);
+		
+		higgs.px = dist(gen);
+		higgs.py = dist(gen);
+		higgs.pz = dist(gen);
+		//higgs.momentum(higgs.px, higgs.py, higgs.pz);
+		//higgs.transMomentum(higgs.px, higgs.py);
+
+		ElementaryParticle* particle1 = new ElementaryParticle();
+		ElementaryParticle* particle2 = new ElementaryParticle();
+		higgs.bosonDecay(particle1, particle2);
+
+		MyFile << particle1->name << "   " << particle1->px << "   " << particle1->py << "   " << particle1->pz << "      " << particle2->name << "   " << particle2->px << "   " << particle2->py << "   " << particle2->pz << "\n";
+	}
+
+	MyFile.close();
 	return 0;
 }
